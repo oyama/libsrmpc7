05805ecedceb7608202e4a39431f39e05db7d6f1
Hiroyuki OYAMA <oyama@module.jp>
2010-10-15 20:46:39 +0900

    support quarqd mode.

--- srmonline.c
+++ srmonline.c
@@ -3,12 +3,18 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <errno.h>
+#include <limits.h>
+#include <inttypes.h>
+#include <time.h>
 
 #include "srmpc7.h"
 
 
 
 static srm_handle_t *current_device = NULL;
+static int flag_quarqd_mode = 0;
+
 
 static void cleanup()
 {
@@ -24,26 +30,46 @@ static void stop()
     current_device = NULL;
 }
 
-/*
-static void _print_hex(unsigned char *src, int len)
-{
-    const char *hex = "0123456789abcdef";
-    int i;
-    char buff[3];
-    for (i = 0; i < len; i++) {
-        buff[0] = hex[src[i] >> 4];
-        buff[1] = hex[src[i]&0x0f];
-        buff[2] = '\0';
-        printf("%02x ", src[i]);
-    }
-    printf("\n"); 
-}
-*/
 
-int main()
+int usage()
 {
+    printf("Usage: srmonline [-hq] [-i INTERVAL]\n"
+           "  -h  show this message.\n"
+           "  -q  set quarqd mode.\n"
+           "  -i  set recording interval, default 1[sec]\n");
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int ch;
     srm_handle_t *srm;
     srm_online_record_t record;
+    int interval = 1;
+    int timestamp;
+
+    while ((ch = getopt(argc, argv, "hqi:")) != -1) {
+        switch (ch) {
+        case 'h':
+            exit(usage());
+            break;
+        case 'q':
+            flag_quarqd_mode = 1;
+            break;
+        case 'i':
+            interval = strtol(optarg, NULL, 0);
+            if (errno == ERANGE || interval == LONG_MAX || interval == LONG_MIN) {
+                fprintf(stderr, "srmonline: cannt set recording interval\n");
+                exit(0);
+            }
+            break;
+        case '?':
+        default:
+            exit(usage());
+        }
+    }
+    argc -= optind;
+    argv += optind;
 
     if ((srm = srm_open(SRM_DEVICE_NAME_PC7)) == NULL) {
         fprintf(stderr, "can't open device \"%s\": %s\n", SRM_DEVICE_NAME_PC7, srm_get_error_message());
@@ -53,24 +79,33 @@ int main()
     signal(SIGINT, stop);
     signal(SIGTRAP, cleanup);
 
-    printf("start online mode\n");
-    printf("Power   HR  Cad Speed   Alt  Temp\n");
+    fprintf(stderr, "start online mode\n");
+    if (!flag_quarqd_mode) printf("Power   HR  Cad Speed   Alt  Temp\n");
     while (current_device != NULL) {
         if (srm_get_online_status(srm, &record)) {
-            printf("%5u %4u %4u %5.1f %5u %5.1f  %u %u\n",
-                record.power,
-                record.heart_rate,
-                record.cadence,
-                (double)(record.speed * 0.1),
-                record.altitude,
-                (double)(record.temperature * 0.1),
-                record.rec1,
-                record.marker);
+            timestamp = time(NULL);
+            if (flag_quarqd_mode) {
+                printf("<HeartRate id=h timestamp=%ul. BPM='%u' />\n", timestamp, record.heart_rate);
+                printf("<Power id=p timestamp=%ul. watts='%u' />\n", timestamp, record.power);
+                printf("<Speed id=s timestamp=%ul. RPM='%.1f'/>\n", timestamp, (double)(record.speed * 0.1));
+                printf("<Cadence id=c timestamp=%ul. RPM='%u' />\n", timestamp, record.cadence);
+            }
+            else {
+                printf("%5u %4u %4u %5.1f %5u %5.1f  %u %u\n",
+                    record.power,
+                    record.heart_rate,
+                    record.cadence,
+                    (double)(record.speed * 0.1),
+                    record.altitude,
+                    (double)(record.temperature * 0.1),
+                    record.rec1,
+                    record.marker);
+            }
             fflush(stdout);
         }
-        sleep(1);
+        sleep(interval);
     }    
-    printf("shutdown online mode\n");
+    fprintf(stderr, "shutdown online mode\n");
     srm_close(srm);
     current_device = NULL;
     return 0;
